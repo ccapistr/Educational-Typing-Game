209dedd3b43632dd5dcd259d011538ac989a99ce
akasha95arts <akeem.roberts@ubalt.com>
2020-04-17 20:44:07 -0400

    Added more word script

---
+++ EducationalGame/Assets/Scripts/Word.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Word 
+{
+    public string word;
+
+    public Word(string _word) //Allows you to set a variable to setup stuff
+    {
+
+    }
+}
---
+++ EducationalGame/Assets/Scripts/WordManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+
+   public List<Word> words;
+
+    private void Start()
+    {
+        AddWord();
+        AddWord();
+        AddWord();
+    }
+
+    public void AddWord()
+    {
+        Word word = new Word("");
+    }
+}
