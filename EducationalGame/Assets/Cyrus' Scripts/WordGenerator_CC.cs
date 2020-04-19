8ee1505ff56c754dc49c8e9e0327d3d295d1b101
AeosA <51251523+AeosA@users.noreply.github.com>
2020-04-18 20:37:52 -0400

    testing branch
    Hopefully no scripts will be deleted

---
+++ EducationalGame/Assets/Cyrus' Scripts/WordGenerator_CC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator_CC : MonoBehaviour
+{
+    public static string[] wordList_CC = { "word", "Talk", "The", "spot", "love", "smash", "anger", "organic" };
+
+    public static string GetRandomWord_CC()
+    {
+        int RandomIndex = Random.Range(0, wordList_CC.Length);
+        string RandomWord = wordList_CC[RandomIndex];
+
+        return RandomWord;
+
+    }
+}
---
+++ EducationalGame/Assets/Cyrus' Scripts/Word_CC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word_CC
+{
+    public string word;
+    private int typeIndex;
+
+    public Word_CC(string _word)
+    {
+        word = _word;
+    }
+
+    public char GetNextLetter()
+    {
+        return word[typeIndex];
+    }
+
+    public void TypeLetter()
+    {
+        typeIndex++;
+    }
+}
