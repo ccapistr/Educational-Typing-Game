ae025a1519bcc0baf8c1a94b08e13f4e16221ee6
AeosA <51251523+AeosA@users.noreply.github.com>
2020-04-19 23:03:22 -0400

    Revert "Revert "Added Typing Code""
    This reverts commit 46630a30971d10294ea8c5fe98d06d336e6ef50b.

---
+++ EducationalGame/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    private static string[] wordList = {"word", "Talk", "The", "spot", "love", "smash", "anger", "organic"};
+
+
+   public static string GetRandomWord() // Method for getting random word from string array
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+}
